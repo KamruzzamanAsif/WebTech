204c92b0ea74efbc791862517165420506e36670
KamruzzamanAsif <asif720kamruzzaman@gmail.com>
2023-03-13 14:44:17 +0600

    iit

--- Angular/test/src/app/item.service.ts
+++ Angular/test/src/app/item.service.ts
@@ -12,7 +12,7 @@ export class ItemService {
   items: Item[] =[
     {id: 1, name: 'Iphone', price: 2000, quantity: 3},
     {id: 2, name: 'mackbook', price: 10000, quantity: 2},
-    {id: 3, name: 'OPPO', price: 300, quantity: 8}
+    {id: 3, name: 'OPPO', price: 300, quantity: 0}
   ];
 
   // function to return item objects
